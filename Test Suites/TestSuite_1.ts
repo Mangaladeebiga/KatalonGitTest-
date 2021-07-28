f27fe35f7b00c1ae8b414e9a436da16fb1fb4f47
Mangaladeebiga <rdeebiga@gmail.com>
2021-07-28 11:18:42 +0530

    qw

---
+++ Test Suites/TestSuite_1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>dc552b08-11af-475b-8232-be0c3192dae7</testSuiteGuid>
+   <testCaseLink>
+      <guid>c27394de-504f-4caa-a596-62bb290f9ab3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Googlesearch</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b1faee46-91d4-4051-b4b1-8eb898839fc3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OrangeHRMLogin</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
